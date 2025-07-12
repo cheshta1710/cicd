5408625c5e873a5bc6ad4456fcae51d69a087ef7
cheshta1710 <cheshtaagarwal17@gmail.com>
2025-07-13 00:23:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import flask 
+
 myapp=flask.Flask(__name__)
-@myapp.route("/info")
-def newinfo():
-    return "this cicd pipeline"
+
+@myapp.route("/")
+def home():
+    return "Welcome to home page."
+@myapp.route("/login")
+def login():
+    return "This is the login page."
+@myapp.route("/aboutus")
+def about():
+    return "This is about us page."
 @myapp.route("/mail")
-def newmail():
-    return "this is second function for my cicd pipeline"
+def mail():
+    return "This is the mail page."
+@myapp.route("/contact")
+def contact():
+    return "This is the contact us page."
+@myapp.route("/links")
+def links():
+    return "This is the links page where you will find all social media links."
 myapp.run(host="0.0.0.0", port=5000)
